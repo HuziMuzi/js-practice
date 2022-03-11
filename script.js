d744caaa02e4a9f7a0cc1a8698a840a8a646c9ec
HuziMuzi <laif17860@mail.ru>
2022-03-11 21:48:16 +0300

    function return value

--- script.js
+++ script.js
@@ -139,3 +139,13 @@ function addFive() {
 
 addThree();
 addFive();
+
+// 18
+// Setup
+let processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+processed = processArg(7)
+// Only change code below this line
