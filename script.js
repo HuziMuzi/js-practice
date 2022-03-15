ccbbdb21fe0af563efd82056bf6d513aa19189ba
HuziMuzi <laif17860@mail.ru>
2022-03-15 20:33:46 +0300

    if else

--- script.js
+++ script.js
@@ -196,3 +196,20 @@ function testNotEqual(val) {
  }
  
  testNotEqual(10);
+
+
+ // 23
+ function testElse(val) {
+	let result = "";
+	// Only change code below this line
+ 
+	if (val > 5) {
+	  result = "Bigger than 5";
+	} else {
+	  result = "5 or Smaller";
+	}
+	// Only change code above this line
+	return result;
+ }
+ 
+ testElse(4);
